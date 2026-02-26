673d352665fef3a208facc1810722d0f1bae6577
Mohammad Shoubash <95358258+moshoubash@users.noreply.github.com>
2026-02-26 04:29:39 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,8 @@ Route::middleware(['auth', '2fa'])->group(function () {
     Route::view('profile', 'profile')->name('profile');
 });
 
+Route::get('backFrom2FA', [Google2FAController::class, 'back'])->name('backFrom2FA');
+
 Route::middleware(['auth'])->group(function () {
     Route::get('/2fa/enable', [App\Http\Controllers\Google2FAController::class, 'enableTwoFactor'])->name('2fa.enable');
     Route::post('/2fa/disable', [App\Http\Controllers\Google2FAController::class, 'disableTwoFactor'])->name('2fa.disable');
